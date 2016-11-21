348e7730791988c63d8dc25b9f159eacf634d864
boybundit <boybundit@hotmail.com>
2016-11-21 17:48:46 +0000

    test

--- node/app/index.js
+++ node/app/index.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Express.js')
 })
 
-app.listen(3000)
+console.log(process.env.PORT)
 
+app.listen(process.env.PORT || 3000)
